ef325a4ddb95dae7e012959a0f7798935534356e
Eric Heikes <eheikes@gmail.com>
2012-11-17 14:26:56 -0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require 'header.html'; ?>
+<h2>Calendar</h2>
+<div class="span3">
+  <ul class="nav nav-list">
+    <label><input type="checkbox" checked> Community Events</label>
+    <label><input type="checkbox" checked> Classes</label>
+    <label><input type="checkbox" checked> Spectator Sports</label>
+    <label><input type="checkbox" checked> Community Events</label>
+    <label>&nbsp;&nbsp;<input type="checkbox" checked> Football</label>
+    <label>&nbsp;&nbsp;<input type="checkbox" checked> Soccer</label>
+    <label>&nbsp;&nbsp;<input type="checkbox" checked> Gymnastics</label>
+    <label>&nbsp;&nbsp;<input type="checkbox" checked> Basketball</label>
+  </ul>
+</div>
+<div class="span3">
+  <h3>Friday</h3>
+  etc
+</div>
+<div class="span3">
+  <h3>Saturday</h3>
+  etc
+</div>
+<div class="span3">
+  <h3>Sunday</h3>
+  etc
+</div>
+<?php require 'footer.html'; ?>
---
+++ services.php
@@ -0,0 +1,17 @@
+<?php require 'header.html'; ?>
+<h2>Restaurants</h2>
+<div class="row">
+  <div class="span3 item-box">
+    <img src="http://lorempixel.com/200/200">
+    <p>Description here</p>
+  </div>
+  <div class="span3 item-box">
+    <img src="http://lorempixel.com/200/200">
+    <p>Description here</p>
+  </div>
+  <div class="span3 item-box">
+    <img src="http://lorempixel.com/200/200">
+    <p>Description here</p>
+  </div>
+</div>
+<?php require 'footer.html'; ?>
